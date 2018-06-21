e79b8f3e5a1734c396517e9b95c033bfcbb5cfb5
kingdido999 <kingdido999@gmail.com>
2018-06-21 11:00:48 -0700

    Uncommit credentials

---
+++ config/credentials.example.js
@@ -0,0 +1,6 @@
+module.exports = {
+  GITHUB_CLIENT_ID: '',
+  GITHUB_CLIENT_SECRET: '',
+  GITHUB_CALLBACK_URL: 'http://localhost:8080/api/user/authViaGitHub/callback',
+  DBURL: 'mongodb://localhost:27017/reforum'
+}
